10dfe9fd181c92a5869c2fbc5aa0427942b100ae
Michael E. Cotterell <mepcotterell@gmail.com>
2016-10-04 13:44:17 -0400

    open example

---
+++ cpp/open.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <cerrno>
+#include <cstring>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+using std::cout;
+using std::endl;
+
+int main(const int argc, const char * argv []) {
+
+  const char * filename = argv[1];
+  int fd = -1;
+
+  fd = open(filename, O_RDONLY | O_CREAT, 0777);
+  cout << "fd = " << fd << " "
+       << "strerror = " << strerror(errno) << endl;
+  
+  return EXIT_SUCCESS;
+} // main
+

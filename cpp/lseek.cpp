3245ab0b9ac32b7c5380c2154bf68e68c553b951
Michael E. Cotterell <mepcotterell@gmail.com>
2016-10-20 10:59:57 -0400

    prepare for lseek example

---
+++ cpp/lseek.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cstring>
+#include <cerrno>
+#include <fcntl.h>
+#include <unistd.h>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+void nope_out(const string & prefix);
+
+int main(const int argc, const char * argv []) {
+
+  if (argc != 2) {
+    cout << "Usage: " << argv[0] << " FILE" << endl;
+    exit(EXIT_FAILURE);
+  } // if
+
+  const char * filename = argv[1];
+  int ofd, cfd;
+
+  if ((ofd = open(filename, O_RDONLY)) == -1) {
+    nope_out("open");
+  } // if
+
+  // DO STUFF HERE
+  
+  if ((cfd = close(ofd)) == -1) {
+    nope_out("close");
+  } // if
+
+  return EXIT_SUCCESS;
+  
+} // main
+
+void nope_out(const string & prefix) {
+  perror(prefix.c_str());
+  exit(EXIT_FAILURE);
+} // nope_out
+

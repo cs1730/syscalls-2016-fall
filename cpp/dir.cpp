78d6739f8451e340ccd3118e6b3adcb60fa9a252
Michael E. Cotterell <mepcotterell@gmail.com>
2016-10-20 16:26:56 -0400

    updated dir example

--- cpp/dir.cpp
+++ cpp/dir.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <sys/types.h>
+#include <dirent.h>
 
 using std::cout;
 using std::endl;
@@ -15,12 +17,30 @@ int main(const int argc, const char * argv []) {
   // turn off buffering for cout
   cout.setf(std::ios::unitbuf);
 
+  const char * dirname = argv[1];
+  DIR * dirp = nullptr;
+  struct dirent * direntp = nullptr;
+
+  if ((dirp = opendir(dirname)) == nullptr) {
+    nope_out("opendir");
+  } // if
+
+  // DO STUFF
+  while ((direntp = readdir(dirp)) != nullptr) {
+    cout << direntp->d_ino << " "
+	 << direntp->d_name << endl;
+  } // while
+
+  if (closedir(dirp) == -1) {
+    nope_out("closedir");
+  } // if
+  
   return EXIT_SUCCESS;
   
 } // main
 
 void nope_out(const string & prefix) {
   perror(prefix.c_str());
-  exit(EXIT_FAILURE);
+  // exit(EXIT_FAILURE);
 } // nope_out
 

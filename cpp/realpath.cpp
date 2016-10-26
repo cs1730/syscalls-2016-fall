cb9569f2cf8af58c326918339d0dd5df6265787e
Michael E. Cotterell <mepcotterell@gmail.com>
2016-10-26 07:21:15 -0400

    added realpath(3) example

---
+++ cpp/realpath.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <climits>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+  char * path = realpath("realpath.cpp", nullptr);
+  cout << path << endl;
+  free(path);
+  return EXIT_SUCCESS;
+} // main
+

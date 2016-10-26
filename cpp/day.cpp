8983c7a9ec9e306ddf1089f358c4823148d6eeca
Michael E. Cotterell <mepcotterell@gmail.com>
2016-10-25 21:26:17 -0400

    reorganized some stuff

--- cpp/day.cpp
+++ cpp/day.cpp
@@ -21,23 +21,18 @@ using namespace std;
 const char days [7][4] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
 
 int julian_day(int y, int m, int d);
+int gregorian_day(int y, int m, int d);
 
 int main(int argc, const char * argv[]) {
 
-  struct tm tm;                         // tm structure
-  char date [11];                       // date string
-    
   int y = atoi(argv[1]);                // year as C string
   int m = atoi(argv[2]);                // month as C string
   int d = atoi(argv[2]);                // day as C string
 
-  sprintf(date, "%d-%d-%d", y, m, d);   // create date string
-  strptime(date, "%Y-%m-%d", &tm);      // extract tm structure
-
-  int gday = tm.tm_wday;                // num days since SUN (Gregorian)
+  int gday = gregorian_day(y, m, d);    // num days since SUN (Gregorian)
   int jday = julian_day(y, m, d);       // num days since SUN (Julian)
 
-  cout << "     Date: " << date << endl;
+  cout << "     Date: " << y << "-" << m << "-" << d << endl;
   cout << "Gregorian: " << gday << " (" << days[gday] << ")" << endl; 
   cout << "   Julian: " << jday << " (" << days[jday] << ")" << endl;
   
@@ -60,4 +55,15 @@ int julian_day(int year, int month, int day) {
     - 32083;
   return (j + 1) % 7;
 } // julian_day
+
+/** 
+ * Compute the Gregorian day.
+ */
+int gregorian_day(int y, int m, int d) {
+  struct tm tm;                         // tm structure
+  char date [11];                       // date string
+  sprintf(date, "%d-%d-%d", y, m, d);   // create date string
+  strptime(date, "%Y-%m-%d", &tm);      // extract tm structure
+  return tm.tm_wday;    
+} // gregorian_day
 

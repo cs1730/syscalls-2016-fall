c514f072c054f2010921c01ca14bec10369044eb
Michael E. Cotterell <mepcotterell@gmail.com>
2016-11-01 18:49:40 -0400

    recursive dir example

---
+++ cpp/dir2.cpp
@@ -0,0 +1,56 @@
+
+#include <ios>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <climits>
+#include <sys/types.h>
+#include <dirent.h>
+#include <unistd.h>
+#include "nope.h"
+
+using std::cout;
+using std::endl;
+using std::string;
+
+void print_dir(const char * dirname, int level = 0);
+
+int main(const int argc, const char * argv []) {
+  cout.setf(std::ios::unitbuf);   // turn off buffering for cout
+  const char * dirname = argv[1]; // get dir name from program argument
+  print_dir(dirname);             // print out the directory
+  return EXIT_SUCCESS;
+} // main
+
+// @TODO probably should handle errors better
+void print_dir(const char * dirname, int level) {
+
+  DIR * dirp = nullptr;
+  struct dirent * direntp = nullptr;
+  char subdirname [PATH_MAX];
+
+  if ((dirp = opendir(dirname)) == nullptr) nope_out("opendir");
+
+  while ((direntp = readdir(dirp)) != nullptr) {
+    const char * ename = direntp->d_name;
+    if ((strcmp(ename, ".") != 0) && (strcmp(ename, "..") != 0)) {
+
+      for (int i = 0; i < level; ++i) cout << "  ";
+      cout << " |- " << ename << endl;
+
+      if (direntp->d_type == DT_DIR) {
+	for (int i = 0; i < level; ++i) cout << "  ";
+	cout << "   \\" << endl;
+	strcpy(subdirname, dirname);
+	strcat(subdirname, "/");
+	strcat(subdirname, ename);
+	print_dir(subdirname, level + 1);
+      } // if
+      
+    } // if
+  } // while
+
+  if (closedir(dirp) == -1) nope_out("closedir");
+  
+} // print_dir
+

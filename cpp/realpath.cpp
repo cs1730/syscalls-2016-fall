d750d3a36361988c873d744587f5bf558fddc1a7
Michael E. Cotterell <mepcotterell@gmail.com>
2016-11-01 21:46:43 -0400

    updated realpath example

--- cpp/realpath.cpp
+++ cpp/realpath.cpp
@@ -1,13 +1,14 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <dirent.h>
 
 using namespace std;
 
-int main() {
-  char * path = realpath("realpath.cpp", nullptr);
+int main(const int argc, const char * argv []) {
+  char path [PATH_MAX];
+  realpath(argv[1], path);
   cout << path << endl;
-  free(path);
   return EXIT_SUCCESS;
 } // main
 

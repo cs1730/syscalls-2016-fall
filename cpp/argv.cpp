f304bc89f6db7c069fcfb60c1f1027443482843a
Michael E. Cotterell <mepcotterell@gmail.com>
2016-11-03 14:44:43 -0400

    argv demo

---
+++ cpp/argv.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main(const int argc, const char * argv []) {
+  for (int i = 0; i < argc; ++i) {
+    cout << argv[i] << endl;
+  } // for
+  return 0;
+} // main

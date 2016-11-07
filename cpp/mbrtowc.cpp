e07f80c411a154fc8c64db1db463ff6715f3093f
Michael E. Cotterell <mepcotterell@gmail.com>
2016-11-06 22:47:46 -0500

    added comments

--- cpp/mbrtowc.cpp
+++ cpp/mbrtowc.cpp
@@ -8,23 +8,23 @@
 const size_t BUFFSIZE = 1024;
 
 int main(const int argc, const char * argv []) {
-  std::setlocale(LC_ALL, "en_US.utf8");
-  const char * filename = argv[1];
-  char buffer [BUFFSIZE];
-  int fd = open(filename, O_RDONLY);
-  int nbytes = 0;
-  int chars = 0;
+  std::setlocale(LC_ALL, "en_US.utf8");       // enable unicode
+  const char * filename = argv[1];            // filename
+  char buffer [BUFFSIZE];                     // read buffer
+  int fd = open(filename, O_RDONLY);          // file descriptor
+  int nbytes = 0;                             // # bytes read
+  int chars = 0;                              // # characters read
   while ((nbytes = read(fd, buffer, BUFFSIZE)) > 0) {
-    std::mbstate_t state = std::mbstate_t();
-    char * ptr = buffer;
-    const char* end = ptr + std::strlen(ptr);
-    int len;
-    wchar_t wc;
+    std::mbstate_t state = std::mbstate_t();  // initialize state
+    char * ptr = buffer;                      // start of sliding window
+    const char* end = ptr + std::strlen(ptr); // end of sliding window
+    int len;                                  // # bytes processed
+    wchar_t wc;                               // multibyte character
     while((len = std::mbrtowc(&wc, ptr, end-ptr, &state)) > 0) {
       std::wcout << "Next " << len << " bytes are the character " << wc << std::endl;
-      ptr += len;
-      chars += 1;
-      if (ptr > end) break;
+      ptr += len;                             // move onto the next one
+      chars += 1;                             // increment character count
+      if (ptr > end) break;                   // don't go past the end
     } // while
   } // while
   std::wcout << "# chars = " << chars << std::endl;

fb0ae022d5afdfd63a7ee5b1074d8e9e39d08af8
Michael E. Cotterell <mepcotterell@gmail.com>
2016-11-07 22:25:37 -0500

    added signal example

---
+++ cpp/signal.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <signal.h>
+
+using std::cout;
+using std::endl;
+
+void my_handler(int signo) {
+  cout << "NOPE!!" << endl;
+} // my_handler
+
+int main() {
+  cout << NULL << endl;
+  signal(SIGINT, (void (*)(int)) 42);
+  // signal(SIGINT, my_handler);
+  // sighandler_t old_handler = signal(SIGINT, SIG_IGN);
+  //  signal(SIGINT, old_handler);
+  // signal(SIGINT, SIG_DFL);
+  while (true);
+  return EXIT_SUCCESS;
+} // main
+

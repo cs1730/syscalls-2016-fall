15de4161bff3340f32df006e34b060717869c607
Michael E. Cotterell <mepcotterell@gmail.com>
2016-11-08 10:16:38 -0500

    added updated mbrtowc example

---
+++ cpp/mbrtowc2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <clocale>
+#include <cstring>
+#include <cwchar>
+#include <cwctype>
+#include <fcntl.h>
+#include <unistd.h>
+
+const size_t BUFFSIZE = 1024;
+//const size_t BUFFSIZE = 8;
+
+// @see http://en.cppreference.com/w/cpp/string/multibyte/mbrtowc
+int main(const int argc, const char * argv []) {
+  std::setlocale(LC_ALL, "en_US.utf8");        // enable unicode
+  const char * filename = argv[1];             // filename
+  char buffer [BUFFSIZE];                      // read buffer
+  int fd = (argc == 1) ? STDIN_FILENO : open(filename, O_RDONLY);
+  size_t nbytes = 0;                           // # bytes read
+  size_t nchars = 0;                           // # characters read
+  size_t nwords = 0;                           // # words read
+  bool inword   = false;
+  while ((nbytes = read(fd, buffer, BUFFSIZE)) > 0) {
+    std::mbstate_t state = std::mbstate_t();   // initialize state
+    size_t len = 0;                            // # bytes processed
+    wchar_t wc = 0;                            // multibyte character
+    size_t sum = 0;                            // sum # bytes processed
+    while((len = std::mbrtowc(&wc, buffer+sum, nbytes-sum, &state)) > 0) {
+      if (len == (size_t) -2) break;           // incomplete wchar
+      if (!iswspace(wc)) {
+	if (!inword) {
+	  inword  = true;
+	  nwords += 1;
+	} // if
+      } else if (inword) {
+	inword = false;
+      } // if
+      std::wcout << "Next " << len << " bytes are the character " << wc << std::endl;
+      sum    += len;                           // accumulate sum
+      nchars += 1;                             // increment character count
+    } // while
+    // handle incomplete wchar at end of buffer by seeking back a little
+    if (sum < nbytes) lseek(fd, sum-nbytes, SEEK_CUR);
+  } // while
+  std::wcout << "nchars = " << nchars << std::endl;
+  std::wcout << "nwords = " << nwords << std::endl;
+  close(fd);
+} // main
+

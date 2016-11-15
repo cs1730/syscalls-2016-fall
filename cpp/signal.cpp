f53306dfb14dc28397a56a07bea19b3fd4a4259b
Michael E. Cotterell <mepcotterell@gmail.com>
2016-11-15 18:14:31 -0500

    updated signal example

--- cpp/signal.cpp
+++ cpp/signal.cpp
@@ -7,9 +7,21 @@
 using std::cout;
 using std::endl;
 
+void my_handler(int signo) {
+  cout << "NOPE!" << endl;
+  switch (signo) {
+  case  SIGINT: cout << "SIGINT"  << endl; break;
+  case SIGQUIT: cout << "SIGQUIT" << endl; break;
+  default:      cout << "WAT?"    << endl; break;
+  } // switch
+} // my_handler
+
 int main() {
-  // TODO setup signal handler for SIGINT (Interrupt Signal)
-  while (true) pause(); // wait for signal
+  signal(SIGINT, my_handler);  // handler for SIGINT (generate with C-c)
+  signal(SIGQUIT, my_handler); // handler for SIGQUIT (generate with C-\)
+  signal(SIGTSTP, SIG_IGN);    // ignore SIGTSTP (generate with C-z)
+  //  while (true) pause(); // wait for signal
+  for (int i = 0; true; ++i) cout << i << endl;
   return EXIT_SUCCESS;
 } // main
 
